2af2f108862936e9f93b6b6213233f668105a9bd
RuslanLazarev <ruslan.lazarev@gmail.com>
2020-04-06 23:00:10 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -52,7 +52,7 @@ namespace HardwareDemo
 
             var info = new ProcessStartInfo("top -b -n 1");
             info.FileName = "/bin/bash";
-            info.Arguments = "-c \"top -b -n 1\"";
+            info.Arguments = " -b \"top -n 1\"";
             info.RedirectStandardOutput = true;
 
             using (var process = Process.Start(info))
